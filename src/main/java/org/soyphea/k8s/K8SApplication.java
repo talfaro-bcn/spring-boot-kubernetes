9702305f8c8a8f9849aad5f3e255a1a3479543e6
talfaro-bcn <114708675+talfaro-bcn@users.noreply.github.com>
2022-10-27 09:23:54 -0300

    Update K8SApplication.java

--- src/main/java/org/soyphea/k8s/K8SApplication.java
+++ src/main/java/org/soyphea/k8s/K8SApplication.java
@@ -6,27 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*
-import javax.crypto.Cipher;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.NoSuchPaddingException;
-*/
+
 @SpringBootApplication
 @Slf4j
 public class K8SApplication implements CommandLineRunner {
 
     @Autowired
     UserConfig userConfig;   
-    /*
-    public static void main(String[] args) {
-        try {
-            Cipher c1 = Cipher.getInstance("DES"); // Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
-        }
-        catch(NoSuchAlgorithmException|NoSuchPaddingException e) { }
-        
-        SpringApplication.run(K8SApplication.class, args);
-    }
-    */
+
     @Override
     public void run(String... args) throws Exception {
         log.info(" Fetched user from k8s ConfigMap <spring-boot-k8s> is :{}", userConfig);
